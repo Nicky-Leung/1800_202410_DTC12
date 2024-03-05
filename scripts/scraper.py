6a9d44cb9e9b5d5101a961665b907abee49e3e36
NICK_THUCYDIDES <nicholas.thucydides@gmail.com>
2024-03-04 17:54:02 -0800

    Add button to item_page.html that redirects to stats.html and displays pseudo statistics

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -23,7 +23,21 @@ url = f"{base_url}minPrice={min_price}&maxPrice={max_price}&query={make}{model}&
 print(url)
 
 
-  
+
+data = {
+    'item_name': ['Item A', 'Item B', 'Item C'],
+    'price_sold': [100, 150, 80],
+    'date': ['2024-03-01', '2024-03-02', '2024-03-03'],
+    'condition': ['New', 'Used', 'New'],
+    'other_info': ['Info 1', 'Info 2', 'Info 3']
+}
+
+df = pd.DataFrame(data)
+
+df.to_csv('scripts/data.csv', index=False)
+
+
+
 
 
 
