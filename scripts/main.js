a78543fa1fd4a1c7c0a54cdb521bfeb773b58a2f
nicky leung <nicky-leung@outlook.com>
2024-03-18 22:03:05 -0700

    fix main moved search function to search.js

--- scripts/main.js
+++ scripts/main.js
@@ -158,15 +158,7 @@ function readFilteredTechItemDB(itemName) {
 }
 
 
-// Add event listener to the search form
-document.getElementById("searchForm").addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent form submission
 
-    let itemName = document.getElementById("itemName").value; // Get item name from form input
-
-    // Call readFilteredTechItemDB with the entered item name
-    readFilteredTechItemDB(itemName);
-})
 
 
 readFilteredTechItemDB();
