6d8235598e6f8dc7ec356e15aac3e7d3fcdf9c51
Jarell <mjnvicencio@gmail.com>
2024-04-07 22:38:09 -0700

    css for profile item final

--- scripts/profile_item.js
+++ scripts/profile_item.js
@@ -22,15 +22,32 @@ function renderItem(item, docId) {
     const itemElement = document.createElement("div");
     itemElement.classList.add("item");
     itemElement.innerHTML = `
-        <div class="favorited-item">
-                <h2>${item.name}</h2>
-                <a href="item_page.html" style="text-decoration: none; color: inherit;">
-                    <img style="max-width:200px" src="${item.imageUrl}" alt="Item Image">
-                    <p style="font-weight: bold;">Current price: ${item.price}</p>
-                    <p>Description: ${item.description}</p>
-                </a>
-                <button class="delete-btn btn btn-warning btn-lg mb-4">Delete Item</button>
+       <div class="favorited-item">
+    <div class="text-decoration-none text-dark">
+        <div class="card-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1 id='name' class="">${item.name}</h1>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <h5 id="price" class="card-title mt-2">Current Price: ${item.price}</h5>
+                    </div>
+                    <div class="col">
+                        <img src="${item.imageUrl}" class="card-img-top mt-2" style="max-width:200px;">
+                    </div>
+                </div>
+                <p id="description" class="card-text mt-4">${item.description}</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             </div>
+        </div>
+    </div>
+    <button class="delete-btn btn btn-warning btn-lg mb-4">Delete Item</button>
+</div>
+
 `; //added div favorited item for the css
     const itemContainer = document.getElementById("itemContainer");
     itemContainer.appendChild(itemElement);
