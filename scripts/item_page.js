2c131e4140bbd153a564feeb85ff05ae8ab53856
Jarell <mjnvicencio@gmail.com>
2024-04-07 23:00:14 -0700

    removed colour black in item details

--- scripts/item_page.js
+++ scripts/item_page.js
@@ -26,11 +26,11 @@ function show_item() {
         <div class="item" id="item" style="display:flex">
             <h2>${name}</h2>
             <div style="margin-left: auto;">
-                <img style="max-width:200px; " src="${imageUrl}" alt="Item Image">
+                <img style="max-width:200px; margin-bottom: 20%;" src="${imageUrl}" alt="Item Image">
             </div>
         </div>
         <div class="card" id="cardi">
-            <p>Price: ${price}<br>Condition: Used<br>Colour: Black</p>
+            <p>Price: ${price}<br>Condition: Used<br></p>
             <p>${description}</p>
             <svg class= "m-1"  id="heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32" onclick="toggleFill(); addToFavorites()">
                 <path id="heartPath" fill="none" stroke="red" stroke-width="2" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
